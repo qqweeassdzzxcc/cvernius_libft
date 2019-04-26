246074abcefd69c9ee11739c64285c9a0f24350c
Forsunka <f0rsunkai9@gmail.com>
2019-04-26 22:26:18 +0300

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -16,39 +16,58 @@ static int	ft_del(int count)
 {
 	int d;
 
-	d = 0;
-	while (count)
+	d = 1;
+	while (count > 1)
 	{
 		d = d * 10;
+		count--;
 	}
+	return (d);
 }
 
 static char	*ft_filling(char *s, int n, int count)
 {
 	int i;
+	int del;
+	char smbl;
 
 	i = 0;
 	if (n < 0)
 	{
 		s[i] = '-';
 		i++;
+		count--;
+		n *= -1;
 	}
-
+	while (count > 0)
+	{
+		del = ft_del(count);
+		smbl = n / del;
+		s[i] = smbl + 48;
+		n = n % del;
+		count--;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
 }
 
 char	*ft_itoa(int n)
 {
 	int count;
+	int tmp;
 	char *s;
 
 	count = 0;
-	while (n != 0)
+	tmp = n;
+	while (tmp != 0)
 	{
-		n = n / 10;
+		tmp = tmp / 10;
 		count++;
 	}
+	count = (n <= 0) ? (count + 1) : count;
 	if (!(s = malloc((sizeof(char)) * (count + 1))))
 		return (NULL);
 	else
-	return (ft_filling(char *s, int n, int count));
+	return (ft_filling(s, n, count));
 }
