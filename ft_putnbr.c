3d9234780104280a604300aff158a1aff1df115e
Forsunka <f0rsunkai9@gmail.com>
2019-04-27 15:16:39 +0300

    ft_putnbr.c

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,6 +1,46 @@
 #include "libft.h"
 
-void ft_putnbr(int n)
+static int	ft_count_digits(int n)
 {
+	int count;
 
+	count = (n == 0) ? 1 : 0;
+	while (n != 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+
+static int	ft_del(int count)
+{
+	int d;
+
+	d = 1;
+	while (count > 1)
+	{
+		d *= 10;
+		count--;
+	}
+	return (d);
+}
+
+void	ft_putnbr(int n)
+{
+	int del;
+	int count_dit;
+	int res;
+
+	count_dit = ft_count_digits(n);
+	if (n < 0)
+		ft_putchar('-');
+	while (count_dit > 0)
+	{
+		del = ft_del(count_dit);
+		res = (n < 0) ? (-1 * (n / del)) : (n / del);
+		ft_putchar(res + 48);
+		n = n % del;
+		count_dit--;
+	}
 }
